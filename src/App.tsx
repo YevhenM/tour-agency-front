aed6655b2490e425f8c1cdabcc002ba773fcba24
YevhenM <marukhnyak.y@gmail.com>
2021-04-09 15:45:24 +0300

    first form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Box, Button, TextField} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Container>
+        <Box>
+          <h2>Hello world app</h2>
+        </Box>
+        <Box m={3}>
+          <TextField id="outlined-basic" label="Login" variant="outlined" />
+        </Box>
+        <Box m={1}>
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </Box>
+        <Box m={1}>
+          <Button variant="contained" >Log in</Button>
+        </Box>
+
+        <Box component="span" m={1}>        
+          
+          
+          <Button variant="contained" color="primary">
+              Sign In
+          </Button>
+        </Box>
+      </Container>
+      
     </div>
   );
 }
